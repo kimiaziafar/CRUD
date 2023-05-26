000f4506881c5c6e28c1875bb963da6e97f178a5
kimiaziafar <133915983+kimiaziafar@users.noreply.github.com>
2023-05-27 01:13:09 +0330

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,9 @@
--- User user/pass
-INSERT INTO users (username, password, enabled)
+-- User user@email.pass/pass
+INSERT INTO bael_users (name, email, password, enabled)
   values ('user',
+    'user@email.com',
     '$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRzgVymGe07xd00DMxs.AQubh4a',
     1);
 
-INSERT INTO authorities (username, authority)
-  values ('user', 'ROLE_USER');
+INSERT INTO authorities (email, authority)
+  values ('user@email.com', 'ROLE_USER');
