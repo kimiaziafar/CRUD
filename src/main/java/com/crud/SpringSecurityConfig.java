b749b920ccb7c20f213c4e2a4213b2b2184a2a92
kimiaziafar <133915983+kimiaziafar@users.noreply.github.com>
2023-05-27 01:07:37 +0330

    Update SpringSecurityConfig.java

--- src/main/java/com/crud/SpringSecurityConfig.java
+++ src/main/java/com/crud/SpringSecurityConfig.java
@@ -1,1 +1,39 @@
+package com.crud.secutity;
 
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.authentication.dao.*;
+import org.springframework.security.config.annotation.AbstractSecurityBuilder;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity)
+      throws Exception {
+        httpSecurity.authorizeRequests()
+          .requestMatchers("/h2-console/**")
+          .permitAll()
+          .anyRequest()
+          .authenticated()
+          .and()
+          .formLogin();
+        
+        httpSecurity.csrf()
+          .ignoringRequestMatchers("/h2-console/**");
+        httpSecurity.headers()
+          .frameOptions()
+          .sameOrigin();
+      AbstractSecurityBuilder<DefaultSecurityFilterChain> http = null;
+	return http.build();
+    }
+    
+    
+}
